c3fc5c00324476b29106bdde72ed8a62635b283c
Andry Ariadi <andryariadi23@gmail.com>
2023-10-17 07:40:30 +0700

    update banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,16 +12,16 @@ const Banner = () => {
     <>
       <div className="section" id="home">
         <div className="container mx-auto">
-          <div>
-            <div className="flex-1 text-center lg:text-start font-secondary">
-              <h1 className="text-[40px] lg:text-[80px] font-bold leading-[0.5] mb-5">
-                Hi! I'm Andry <span>Ariadi</span>
+          <div className="header flex flex-col lg:flex-row lg:items-center">
+            <div className="flex-1 text-center lg:text-start font-secondary mt-8 lg:mt-0">
+              <h1 className="text-[37px] lg:text-[75px] font-bold leading-[0.8] mb-5">
+                Hi! I'm <span className="text-gradient">Andry Ariadi</span>
               </h1>
-              <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <div className="mb-6 text-[30px] lg:text-[55px] font-secondary font-semibold uppercase leading-[1]">
                 <span className="text-white mr-3">I am a</span>
                 <TypeAnimation sequence={["Software Engineering", 2000, "Frontend Developer", 2000]} speed={50} className="text-accent" wrapper="span" repeat={Infinity} />
               </div>
-              <p className="mb-3">Welcome to my personal website</p>
+              <p className="mb-3">Welcome to My personal website</p>
               <div className="flex justify-center lg:justify-start text-xl lg:text-3xl">
                 <div>
                   <a href="https://drive.google.com/file/d/1gzxxRYA1Zb6si9Z10HI36q4LjzIJhh6H/view?usp=drive_link" target="_blank">
@@ -41,8 +41,8 @@ const Banner = () => {
                 </div>
               </div>
             </div>
-            <div className="">
-              <img src={Andry} alt="Andry" />
+            <div className="mx-auto lg:mx-0">
+              <img src={Andry} alt="Andry" width={300} height={300} className="img-header brightness-75 contrast-90 hue-rotate-15 bg-gradient-to-r from-purple-700 to-sky-600" />
             </div>
           </div>
         </div>
