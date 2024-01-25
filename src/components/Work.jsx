19716ee8a3d503f211198a2f4f485966c7796989
Andry Ariadi <andryariadi23@gmail.com>
2024-01-26 06:24:54 +0700

    update work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,9 @@ import { fadeIn } from "../variants";
 import img1 from "../assets/web-portfolio.png";
 import img2 from "../assets/SQR.png";
 import img3 from "../assets/course.png";
-import img4 from "../assets/movie.png";
+import img4 from "../assets/evently.png";
+// import img5 from "../assets/cms2.png";
+// import img6 from "../assets/anime.png";
 
 const Work = () => {
   return (
@@ -24,7 +26,7 @@ const Work = () => {
                   <span className="text-gradient">Web Application</span>
                 </div>
                 <div className="absolute -bottom-full left-10 group-hover:bottom-12 transition-all duration-700 z-50">
-                  <span className="text-3xl text-white">iMovie</span>
+                  <span className="text-3xl text-white">Evently</span>
                 </div>
               </div>
               <div className="group relative overflow-hidden border-2 w-[400px] lg:w-[450px] border-white/50 rounded-xl">
@@ -37,6 +39,16 @@ const Work = () => {
                   <span className="text-3xl text-white">SQR</span>
                 </div>
               </div>
+              {/* <div className="group relative overflow-hidden border-2 w-[400px] lg:w-[450px] border-white/50 rounded-xl">
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                <img className="group-hover:scale-150 transition-all duration-300 w-[400px] lg:w-[450px] h-[270px]" src={img5} alt="Course" />
+                <div className="absolute -bottom-full left-10 group-hover:bottom-20 transition-all duration-500 z-50">
+                  <span className="text-gradient">Web Application</span>
+                </div>
+                <div className="absolute -bottom-full left-10 group-hover:bottom-12 transition-all duration-700 z-50">
+                  <span className="text-3xl text-white">CMS Dashboard</span>
+                </div>
+              </div> */}
             </motion.div>
             <motion.div variants={fadeIn("left", 0.5)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.3 }} className="flex-1 flex flex-col gap-y-5">
               <div className="group relative overflow-hidden border-2 w-[400px] lg:w-[450px] border-white/50 rounded-xl">
@@ -59,6 +71,16 @@ const Work = () => {
                   <span className="text-3xl text-white">Course</span>
                 </div>
               </div>
+              {/* <div className="group relative overflow-hidden border-2 w-[400px] lg:w-[450px] border-white/50 rounded-xl">
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                <img className="group-hover:scale-110 transition-all duration-300 w-[400px] lg:w-[450px] h-[270px]" src={img6} alt="Course" />
+                <div className="absolute -bottom-full left-10 group-hover:bottom-20 transition-all duration-500 z-50">
+                  <span className="text-gradient">Website</span>
+                </div>
+                <div className="absolute -bottom-full left-10 group-hover:bottom-12 transition-all duration-700 z-50">
+                  <span className="text-3xl text-white">AnimeList</span>
+                </div>
+              </div> */}
             </motion.div>
           </div>
         </div>
